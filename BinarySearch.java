cdfcce62289b4a779546c04d7be94b6b8d15b10a
AtharvaSrivastava11 <150154780+AtharvaSrivastava11@users.noreply.github.com>
2024-01-16 11:49:05 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+    // array should be sorted
+    public static int searchBin(int num[], int key)
+    {
+        int start = 0;
+        int end = num.length-1;
+        
+        while(start <= end)
+        {
+           int mid=(start+end)/2;
+           if(num[mid] == key)
+           {
+            return mid;
+           }
+
+           if(num[mid] < key )
+           {
+              start = mid+1;
+           } else {
+            end = mid-1;
+           }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int num[] = {2,4,6,8,10,12,14,16,18,20};
+        int key = 10;
+        
+        System.out.println("Index for key is: " + searchBin(num,key));
+    }
+}
